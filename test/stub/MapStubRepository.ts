7d7d38f139efc17c6e49f6af1be380baf360526b
iq3addLi <iq3@addli.co.jp>
2018-12-25 18:12:57 +0900

    Allow empty functions

--- test/stub/MapStubRepository.ts
+++ test/stub/MapStubRepository.ts
@@ -10,18 +10,23 @@ export default class MapStubRepository implements MapRepository {
     public load = (handler: (view: HTMLElement, error?: Error) => void ) => {
 
     }
+
     public isLoaded = () => {
         return true
     }
+
     public refresh = () => {
 
     }
+
     public setMarkers = ( locations: Location[] ) => {
 
     }
+
     public setMarkerClickHandler = ( hanlder: ( location: Location ) => void ) => {
 
     }
+
     public setInfoWindowContent = (content: HTMLElement) => {
 
     }
